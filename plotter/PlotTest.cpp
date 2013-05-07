c8c46315a4faaba74a80cd5bd50d59e096dbdc40
Robert <robert@robert-netbook.(none)>
2013-05-07 13:07:00 +0200

    plotter-Klasse erstellt

---
+++ plotter/PlotTest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <stdlib.h>
+#include <vector>
+#include <functional>
+
+#include "plotter.h"
+
+double testFkt (double x)
+{
+    double a = 1.;
+    double b = 1.;
+    return a*x*x + b*x;
+}
+
+//main
+int main () {
+	int N = 100;
+	std::string pfad = "./";
+	
+	plotter plt;
+	plt.setxrange(0,6);
+	
+	plt.plot(&testFkt);
+	return 0;
+}
